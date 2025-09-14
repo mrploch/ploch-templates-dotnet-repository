72bcaa9c29d85f714715efd61466f3cd4fb2ad3a
Krzysztof Ploch <kris@ploch.dev>
2025-09-14 05:18:26 +0200

    Commited again the nuget and sonar scripts

--- scripts/publish-nugetorg.ps1
+++ scripts/publish-nugetorg.ps1
@@ -1,1 +1,2 @@
 dotnet nuget push **/*.nupkg --api-key $env:NUGET_TOKEN --source https://api.nuget.org/v3/index.json
+
--- scripts/run-sonar-build-test.ps1
+++ scripts/run-sonar-build-test.ps1
@@ -5,3 +5,4 @@ $VerbosePreference = 'Continue'
 . ./build-helpers.ps1
 Clear-Solution -solutionDirectory "../"
 Build-Solution -solutionFile "../Ploch.DotNetRepository.sln" -collectCoverage $true -sonarCloudAnalysis $true -sonarCloudProjectKey "mrploch_ploch-templates-dotnet-repository" -sonarCloudOrganization "mrploch" -sonarToken $env:SONARCLOUD_TOKEN_PlochDotNetRepositoryTemplate
+
