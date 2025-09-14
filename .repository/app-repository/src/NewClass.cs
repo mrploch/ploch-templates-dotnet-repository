c14f8d87790916f0e9de6866a850cb7e03da4528
Krzysztof Ploch <kris@ploch.dev>
2025-09-14 03:37:37 +0200

    .editorconfig update and code cleanup

---
+++ .repository/app-repository/src/NewClass.cs
@@ -0,0 +1,24 @@
+namespace Ploch.EditorConfig.FormattingCheck
+{
+    public class NewClass
+    {
+        public void NewMethod()
+        {
+            // Implementation goes here
+        }
+
+        public NewClass()
+        {
+            // Constructor implementation
+        }
+
+        private static void PrivateStaticMethod()
+        {
+            // Implementation goes here
+        }
+
+        public int NewProperty { get; set; }
+
+        public static void StaticMethod() { }
+    }
+}
